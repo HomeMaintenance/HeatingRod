fc1da3187363fa7d92d14c88d418eb1503f8efca
Planet-Moon <topwastaken@abwesend.de>
2022-07-29 18:11:25 +0200

    add log

--- HeatingRod.cpp
+++ HeatingRod.cpp
@@ -112,6 +112,7 @@ bool HeatingRod::allow_power(float power){
     else{
         log("Inbetween hysteresis limits");
         if(first_allow){
+            log("\tFirst allow");
             first_allow = false;
             return turn_on();
         }
